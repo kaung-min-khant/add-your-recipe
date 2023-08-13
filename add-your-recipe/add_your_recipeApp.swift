c04ee72de74c389bfa28e0f7fb85956eaa4eb69f
kaung-min-khant <1kaungminkhant1@gmail.com>
2023-08-13 13:26:03 -0700

    Initial Commit

---
+++ add-your-recipe/add_your_recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  add_your_recipeApp.swift
+//  add-your-recipe
+//
+//  Created by Kaung Min Khant on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct add_your_recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
